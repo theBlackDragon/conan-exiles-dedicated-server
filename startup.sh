0e127df9c371ed7a920bb349445529009e20bcd2
Bert Geens <bert@lair.be>
2021-02-24 20:28:53 +0100

    validate install on startup
    Validate the application on startup to avoid buildup of crud after
    updates, and to enable cleanup of folders without having to start
    over entirely.

--- startup.sh
+++ startup.sh
@@ -21,7 +21,7 @@ Updating application
 -------------------------------------
 "
 set -x
-su steam -c "${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} +quit"
+su steam -c "${STEAMCMDDIR}/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir ${STEAMAPPDIR} +app_update ${STEAMAPPID} validate +quit"
 
 echo "
 ------------------------------------
